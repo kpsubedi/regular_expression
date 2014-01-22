0d3938f39979382b938a5fb2fce8cecc936948c0
Kul Prasad Subedi <subedi.kulprasad@gmail.com>
2014-01-22 11:59:58 -0600

    Second Commit

---
+++ java_example/Example.java
@@ -0,0 +1,20 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Example {
+	public static void main(String[] args)	{
+		String str = "var x = 100;";
+		Pattern pattern = Pattern.compile("var");
+		Matcher matcher = pattern.matcher(str);
+		if (matcher.find()){
+			System.out.println("Matches found: " + matcher.group());
+		}
+		else {
+
+			System.out.println("No matches found");
+			}
+
+		}
+	}
+
+
